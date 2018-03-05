e2ac63e5d42c90fca2f93ab5b274ebcf883377c2
Kanardheure <brehier.colin@gmail.com>
2018-03-05 07:07:14 +0100

    Création d'une fenêtre pygame

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # -*- coding:Utf-8 -*-
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+window = pygame.display.set_mode((500, 500))
+
+
+game = 1
+while game:
+    game = int(input())
